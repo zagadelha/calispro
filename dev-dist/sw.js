d7c0a84544f5b6005cbbdc06c242984a513514a4
marciogadelha <zagadelha@gmail.com>
2025-12-16 23:51:30 +0000

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o93ibp72ob8"
+    "revision": "0.6r06pum3lig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
