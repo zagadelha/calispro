34e02974c60c38a9ea3299a641561987ac8eee93
marciogadelha <zagadelha@gmail.com>
2026-02-05 18:52:48 +0000

    feat: Add exercise media assets and implement service worker for PWA capabilities.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.792g9aq0v8g"
+    "revision": "0.abj5l3c37c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
